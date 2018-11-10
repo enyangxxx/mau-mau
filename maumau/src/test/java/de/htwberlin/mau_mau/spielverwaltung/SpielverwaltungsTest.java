3f778c80274bb5625b8dab2faa8a2288ea6d3fc4
enyang <32799307+enyangxxx@users.noreply.github.com>
2018-11-10 15:46:19 +0100

    javadoc added

--- maumau/src/test/java/de/htwberlin/mau_mau/spielverwaltung/SpielverwaltungsTest.java
+++ maumau/src/test/java/de/htwberlin/mau_mau/spielverwaltung/SpielverwaltungsTest.java
@@ -8,7 +8,6 @@ import de.htw.berlin.maumau.kartenverwaltung.kartenverwaltungsInterface.Karte;
 import de.htw.berlin.maumau.spielerverwaltung.spielerverwaltungsInterface.Spieler;
 import de.htw.berlin.maumau.spielverwaltung.spielverwaltungsInterface.ISpielverwaltung;
 import de.htw.berlin.maumau.spielverwaltung.spielverwaltungsInterface.MauMauSpiel;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -40,44 +39,59 @@ public class SpielverwaltungsTest {
     public ExpectedException exceptionRule = ExpectedException.none();
 
     @Before
-    public void setUp(){
+    public void setUp() {
     }
 
+    /**
+     * Teste die Funktionalität, ein neues Spiel zu starten, indem eine Spielerliste mit 2 Spielern übergeben wird.
+     */
     @Test
-    void testNeuesSpielStartenMitSpielerliste(){
+    void testNeuesSpielStartenMitSpielerliste() {
         spielerliste = new ArrayList<Spieler>();
         spielerliste.add(ingo);
         spielerliste.add(enyang);
         MauMauSpiel neuesSpiel = spielverwaltung.neuesSpielStarten(spielerliste);
-        assertEquals("Die erste Runde des neuen Spiels muss 1 sein",1,neuesSpiel.getRunde());
-        assertNotNull("Das neue Spiel muss erstellt worden sein.",neuesSpiel);
+        assertEquals("Die erste Runde des neuen Spiels muss 1 sein", 1, neuesSpiel.getRunde());
+        assertNotNull("Das neue Spiel muss erstellt worden sein.", neuesSpiel);
     }
 
+    /**
+     * Teste die Funktionalität, ein neues Spiel zu starten, indem eine leere Spielerliste übergeben wird.
+     */
     @Test
-    void testNeuesSpielStartenMitSpielerlisteOhneSpieler(){
+    void testNeuesSpielStartenMitSpielerlisteOhneSpieler() {
         exceptionRule.expect(KeineSpielerException.class);
         spielerliste = new ArrayList<Spieler>();
         spielverwaltung.neuesSpielStarten(spielerliste);
     }
 
+    /**
+     * Teste die Funktionalität, ein neues Spiel zu starten, indem keine Spielerliste übergeben wird.
+     */
     @Test
-    void testNeuesSpielStartenOhneSpielerliste(){
+    void testNeuesSpielStartenOhneSpielerliste() {
         exceptionRule.expect(KeineSpielerException.class);
         spielverwaltung.neuesSpielStarten(spielerliste);
     }
 
+    /**
+     * Teste die Funktionalität, eine neue Runde im Spiel zu starten, angefangen mit der 1. Runde.
+     */
     @Test
-    void testNeueRundeStarten(){
+    void testNeueRundeStarten() {
         spielerliste.add(ingo);
         MauMauSpiel spiel = new MauMauSpiel(spielerliste);
         int alteRunde = spiel.getRunde();
         spielverwaltung.neueRundeStarten(spiel);
         int neueRunde = spiel.getRunde();
-        assertEquals("Neue Runde muss um 1 größer sein als die alte Runde",alteRunde + 1,neueRunde);
+        assertEquals("Neue Runde muss um 1 größer sein als die alte Runde", alteRunde + 1, neueRunde);
     }
 
+    /**
+     * Teste die Funktionalität, eine neue Karte zu ziehen.
+     */
     @Test
-    void testKarteZiehen(){
+    void testKarteZiehen() {
         spielerliste.add(ingo);
         Spieler spieler = spielerliste.get(0);
 
@@ -89,26 +103,32 @@ public class SpielverwaltungsTest {
         int neueAnzahlKartenInHand = spieler.getHand().size();
         int neueKartenstapelMenge = stapel.size();
 
-        assertEquals("Die Hand muss um 1 Karte erweitert sein", alteAnzahlKartenInHand + 1,neueAnzahlKartenInHand);
+        assertEquals("Die Hand muss um 1 Karte erweitert sein", alteAnzahlKartenInHand + 1, neueAnzahlKartenInHand);
         assertEquals("Der Kartenstapel muss um 1 Karte verringert sein", alteKartenstapelMenge - 1, neueKartenstapelMenge);
     }
 
+    /**
+     * Teste die Funktionalität, eine neue Karte ohne Angabe eines Spielers zu ziehen.
+     */
     @Test
-    void testKarteZiehenOhneSpieler(){
+    void testKarteZiehenOhneSpieler() {
         exceptionRule.expect(KeineSpielerException.class);
         spielerliste.add(ingo);
         spielverwaltung.karteZiehen(null, stapel);
     }
 
+    /**
+     * Teste die Funktionalität, zwei Karten zu ziehen.
+     */
     @Test
-    void testZweiKartenZiehen(){
+    void testZweiKartenZiehen() {
         spielerliste.add(ingo);
         Spieler spieler = spielerliste.get(0);
 
         int alteAnzahlKartenInHand = spieler.getHand().size();
         int alteKartenstapelMenge = stapel.size();
 
-        spielverwaltung.karteZiehen(spieler, stapel,2);
+        spielverwaltung.karteZiehen(spieler, stapel, 2);
 
         int neueAnzahlKartenInHand = spieler.getHand().size();
         int neueKartenstapelMenge = stapel.size();
@@ -118,15 +138,21 @@ public class SpielverwaltungsTest {
 
     }
 
+    /**
+     * Teste die Funktionalität, zwei neuen Karten ohne Angabe eines Spielers zu ziehen.
+     */
     @Test
-    void testZweiKartenZiehenOhneSpieler(){
+    void testZweiKartenZiehenOhneSpieler() {
         exceptionRule.expect(KeineSpielerException.class);
         spielerliste.add(ingo);
         spielverwaltung.karteZiehen(null, stapel, 2);
     }
 
+    /**
+     * Teste die Funktionalität, eine Karte zu legen.
+     */
     @Test
-    void testKarteLegen(){
+    void testKarteLegen() {
         spielerliste.add(ingo);
         Spieler spieler = spielerliste.get(0);
 
@@ -144,8 +170,11 @@ public class SpielverwaltungsTest {
 
     }
 
+    /**
+     * Teste die Funktionalität, eine Karte zu legen, wenn vorher ein Wunschtyp durch Bube festgelegt wurde.
+     */
     @Test
-    void testBubeLegen(){
+    void testBubeLegen() {
         spielerliste.add(ingo);
 
         Spieler spieler = spielerliste.get(0);
@@ -164,55 +193,73 @@ public class SpielverwaltungsTest {
 
     }
 
+    /**
+     * Teste die Funktionalität, die letzte Karte des Ablagestapels zu ermitteln.
+     */
     @Test
-    void testLetzteKarteVomAblagestapelErmitteln(){
+    void testLetzteKarteVomAblagestapelErmitteln() {
         Karte letzteKarte = spielverwaltung.letzteKarteErmitteln(stapel);
-        assertEquals("Die letzte Karte soll PIK/BUBE sein", letzteKarte, stapel.get(stapel.size()-1));
+        assertEquals("Die letzte Karte soll PIK/BUBE sein", letzteKarte, stapel.get(stapel.size() - 1));
     }
 
+    /**
+     * Teste die Funktionalität, die letzte Karte im leeren Ablagestapel zu ermitteln.
+     */
     @Test
-    void testLetzteKarteInLeererListeErmitteln(){
+    void testLetzteKarteInLeererListeErmitteln() {
         exceptionRule.expect(KeineKarteException.class);
         spielverwaltung.letzteKarteErmitteln(new ArrayList<Karte>());
     }
 
+    /**
+     * Teste die Funktionalität zu prüfen, ob ein Spieler MauMau gerufen hat und ob er zur Strafe 2 Karten gezogen hat.
+     */
     @Test
-    void testMauMauPruefenWennTrue(){
+    void testMauMauPruefenWennTrue() {
         ingo.setHatMauGerufen(true);
         int alteKartenstapelMenge = stapel.size();
-        spielverwaltung.maumauPruefen(ingo,stapel);
+        spielverwaltung.maumauPruefen(ingo, stapel);
         int neueKartenstapelMenge = stapel.size();
 
-        assertEquals("Der Kartenstapel soll sich nicht verrringert haben, wenn MauMau gerufen wurde",alteKartenstapelMenge,neueKartenstapelMenge);
+        assertEquals("Der Kartenstapel soll sich nicht verrringert haben, wenn MauMau gerufen wurde", alteKartenstapelMenge, neueKartenstapelMenge);
     }
 
+    /**
+     * Teste die Funktionalität zu prüfen, ob ein Spieler MauMau gerufen hat und ob er zur Strafe 2 Karten gezogen hat.
+     */
     @Test
-    void testMauMauPruefenWennFalse(){
+    void testMauMauPruefenWennFalse() {
         ingo.setHatMauGerufen(false);
 
         int alteKartenstapelMenge = stapel.size();
         int alteMengeInHand = ingo.getHand().size();
 
-        spielverwaltung.maumauPruefen(ingo,stapel);
+        spielverwaltung.maumauPruefen(ingo, stapel);
 
         int neueKartenstapelMenge = stapel.size();
         int neueMengeInHand = ingo.getHand().size();
 
-        assertEquals("Die Hand soll sich um 2 Karten erweitert haben, wenn MauMau nicht gerufen wurde",alteMengeInHand + 2,neueMengeInHand);
-        assertEquals("Der Kartenstapel soll sich um 2 Karten verrringert haben, wenn MauMau nicht gerufen wurde",alteKartenstapelMenge - 2,neueKartenstapelMenge);
+        assertEquals("Die Hand soll sich um 2 Karten erweitert haben, wenn MauMau nicht gerufen wurde", alteMengeInHand + 2, neueMengeInHand);
+        assertEquals("Der Kartenstapel soll sich um 2 Karten verrringert haben, wenn MauMau nicht gerufen wurde", alteKartenstapelMenge - 2, neueKartenstapelMenge);
     }
 
+    /**
+     * Teste die Funktionalität, MauMau zu rufen.
+     */
     @Test
-    void testMauMauRufen(){
+    void testMauMauRufen() {
         ingo.setHatMauGerufen(false);
         spielverwaltung.maumauRufen(ingo);
 
-        assertTrue("MauMauGerufen soll true sein",ingo.hatMauGerufen());
+        assertTrue("MauMauGerufen soll true sein", ingo.hatMauGerufen());
     }
 
+    /**
+     * Teste die Funktionalität, Minuspunkte anhand der Karten in seiner Hand zu ermitteln.
+     */
     @Test
-    void testMinuspunkteBerechnen(){
-        assertEquals("",19, spielverwaltung.minuspunkteBerechnen(hand));
+    void testMinuspunkteBerechnen() {
+        assertEquals("", 19, spielverwaltung.minuspunkteBerechnen(hand));
     }
 
 
